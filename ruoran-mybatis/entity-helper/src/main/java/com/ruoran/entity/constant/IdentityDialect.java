3343ecede97d3e1820d81fd06f071a34c54b9816
evan <1006296569@qq.com>
2015-08-27 17:32:14 -0500

    none

--- ruoran-mybatis/entity-helper/src/main/java/com/ruoran/entity/constant/IdentityDialect.java
+++ ruoran-mybatis/entity-helper/src/main/java/com/ruoran/entity/constant/IdentityDialect.java
@@ -1,5 +1,10 @@
 package com.ruoran.entity.constant;
 
+import com.ruoran.mybatis.mapper.MapperHelper.IdentityDialect;
+
+/**
+ * IDENTITY的可选值
+ */
 public enum IdentityDialect
 {
 	DB2("VALUES IDENTITY_VAL_LOCAL()"), MYSQL("SELECT LAST_INSERT_ID()"), SQLSERVER("SELECT SCOPE_IDENTITY()"), CLOUDSCAPE("VALUES IDENTITY_VAL_LOCAL()"), DERBY("VALUES IDENTITY_VAL_LOCAL()"), HSQLDB("CALL IDENTITY()"), SYBASE("SELECT @@IDENTITY"), DB2_MF("SELECT IDENTITY_VAL_LOCAL() FROM SYSIBM.SYSDUMMY1"), INFORMIX("select dbinfo('sqlca.sqlerrd1') from systables where tabid=1");
@@ -57,4 +62,5 @@ public enum IdentityDialect
 		}
 		return returnValue;
 	}
+	
 }
